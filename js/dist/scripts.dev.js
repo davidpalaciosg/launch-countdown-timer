8d9fd255d8805f6dc959e321223a24ab678ca105
David Enrique Palacios Garcia <paladavid@hotmail.com>
2021-04-15 12:59:39 -0500

    First structure done, js added

---
+++ js/dist/scripts.dev.js
@@ -0,0 +1,36 @@
+"use strict";
+
+$(document).ready(function () {
+  //Create add days function
+  function addDays(date, days) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
+  }
+
+  var countDownDate = new Date(); //Launching on 14 days
+
+  countDownDate = addDays(countDownDate, 14); // Update the count down every 1 second
+
+  var x = setInterval(function () {
+    // Get today's date and time
+    var now = new Date().getTime(); // Find the distance between now and the count down date
+
+    var distance = countDownDate - now; // Time calculations for days, hours, minutes and seconds
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+    var minutes = Math.floor(distance % (1000 * 60 * 60) / (1000 * 60));
+    var seconds = Math.floor(distance % (1000 * 60) / 1000); // Display the result
+
+    $("#days-counter").html(days);
+    $("#hours-counter").html(hours);
+    $("#minutes-counter").html(minutes);
+    $("#seconds-counter").html(seconds); // If the count down is finished, write some text
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
+});
